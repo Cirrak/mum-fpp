68295583f6017c647f2b1c2ac545b632ffe79040
Tesfay <Tesfay@172.17.2.34>
2016-12-05 00:11:52 -0600

    added type casting example

---
+++ Lab02/src/edu/mum/cs/cs390/lab/lab03/Prog03.java
@@ -0,0 +1,8 @@
+package edu.mum.cs.cs390.lab.lab03;
+
+public class Prog03 {
+	public static void main(String[] args){
+		float a = 1.27f, b = 3.881f, c = 9.6f;	
+		System.out.println((int) (a + b + c) + "\n" + Math.round(a + b + c));		
+	}
+}
